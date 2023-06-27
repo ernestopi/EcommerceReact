4da979c488e5b22dcfdedd8193589a62b8838019
Miguel Ernesto <90855907+ernestopi@users.noreply.github.com>
2023-06-27 16:12:28 -0300

    segundo commit

---
+++ src/screens/tabs/Notification.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Header from '../../common/Header';
+
+const Notification = () => {
+  return (
+    <View>
+      <Header title={'Notificações'}/>
+      
+    </View>
+  );
+};
+
+export default Notification;
